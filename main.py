1ef22b3d346c1be20120222f727c1cca3b7db733
Миха Пет <techie.mikhail@outlool.com>
2019-03-16 03:01:53 +0300

    Немного подравить

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from SocialNetwork import SocialNetwork
 
 def find(network:SocialNetwork, user, count):
-    user_friends = network.network.vertices[user].connected.keys()
+    userFriends = network.network.vertices[user].connected.keys()
     strangers = 0
     for man in network.network:
         if man.key != user and user not in network.network.vertices[man.key]:
-            count1 = 0
-            man_friends = network.network.vertices[man.key].connected.keys()
-            for f in user_friends:
-                if f in man_friends:
-                    count1 += 1
-            if count1 == count:
+            quantity = 0
+            manFriends = network.network.vertices[man.key].connected.keys()
+            for f in userFriends:
+                if f in manFriends:
+                    quantity += 1
+            if quantity == count:
                 strangers += 1
     return strangers
 
