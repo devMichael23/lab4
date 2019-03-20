c420a5cb0dffd6d350af998f951c1638d9e50334
devMichael23 <techie.mikhail@outlook.com>
2019-03-21 01:32:05 +0300

    Реализован нормальный вывод без ошибок

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -1,48 +1,25 @@
-from Graph import Graph, Vertex
 import random
 import networkx as nx
 
 
 class SocialNetwork:
-    def __init__(self, count):
-        self.count = count
-        self.graph = Graph()
+    def __init__(self, countUser, countEdge):
+        self.countUser = countUser
+        self.countEdge = countEdge
         self.Graph = nx.Graph()
-        for i in range(1, self.count+1):
-            self.graph.addVertex(i)
-            self.Graph.add_node(str(i))
-        for i in range(1, self.count+1):
-            if self.graph.vertices[i].getCount() <= 2:
-                num = self.pickNumber(4)
-            else:
+        for i in range(1, self.countUser+1):
+            self.Graph.add_node(i)
+        for i in range(1, self.countUser+1):
+            leng = (2*countEdge)/(countUser*(countUser-1))
+            if leng >= 1:
                 continue
-            for j in range(1, num+1):
-                ran = random.randint(1, self.count)
-                while ran in self.graph.vertices[i]:
-                    ran = random.randint(1, self.count)
+            else:
+                ran = random.randint(1, self.countUser)
+                while ran in self.Graph[i]:
+                    ran = random.randint(1, self.countUser)
                 while ran == i:
-                    ran = random.randint(1, self.count)
-                if self.graph.vertices[i].getCount() > 3:
-                    break
+                    ran = random.randint(1, self.countUser)
+                if leng > 3:
+                    continue
                 else:
-                    self.graph.addEdge(i, ran)
-                    self.graph.addEdge(ran, i)
-                    self.Graph.add_edge(str(i), str(ran))
-
-    def getVertices(self, key):
-        return self.graph.vertices[key]
-
-    def getVertexForKey(self, key):
-        return self.graph.vertices[key].connected.keys()
-
-    def getGraph(self):
-        return self.graph
-
-    def getDrawGraph(self):
-        return self.Graph
-
-    def pickNumber(self, last):
-        return int(random.randint(1, last))
-
-    def getSize(self):
-        return self.count
+                    self.Graph.add_edge(i, ran)
--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from SocialNetwork import SocialNetwork
 import matplotlib.pyplot as plt
 import networkx as nx
 
-def find(network:SocialNetwork, user, count)->(list, int):
-    userFriends = network.getVertexForKey(user)
-    strangerCount = 0
+countUser = 250
+
+def findN(network:SocialNetwork, user, count):
+    userFriends = network.Graph[user]
     strangers = []
-    for man in network.getGraph():
-        if man.key != user and user not in network.getVertices(man.key):
-            quantity = 0
-            manFriends = network.getVertexForKey(man.key)
-            for f in userFriends:
-                if f in manFriends:
-                    quantity += 1
-            if quantity == count:
-                strangerCount += 1
-                strangers.append(man.key)
-    return strangers, strangerCount
+    for man in network.Graph.graph:
+        for manKey in man:
+            if manKey != user and manKey not in userFriends:
+                quantity = 0
+                manFriends = man
+                for f in userFriends:
+                    if f in manFriends:
+                        quantity += 1
+                if quantity == count:
+                    strangers.append(manKey)
+    return strangers
 
 def listOfColor(lst:list, count)->list:
     lstColors = []
@@ -32,15 +33,14 @@ def listOfColor(lst:list, count)->list:
     return lstColors
 
 
-net = SocialNetwork(250)
-for i in net.getGraph():
-    print(i)
+net = SocialNetwork(countUser, 4)
 
-print()
-print("Количество незнакомцев с заданным количеством общих знакомых: ", find(net, 1, 1))
+# print()
+# print("Количество незнакомцев с заданным количеством общих знакомых: ", find(net, 1, 1))
 
-lstColor, count = find(net,1,1)
+# lstColor = find(net,1,1)
+print(findN(net, 1,1))
 
-nx.draw(net.getDrawGraph(), node_color=listOfColor(lstColor, 250), edge_color='blue', node_size=300, font_weight='normal', font_size=7, with_labels=True)
+nx.draw(net.getGraph(), node_size=300, font_weight='normal', font_size=7, with_labels=True)
 plt.show()
 
