5829bc1bb27641b291a4fd883f11d479ab8a43e9
pahanova <pahan470@outlook.com>
2019-03-24 12:10:04 +0300

    Добавлена презентация и отредачены комменты

--- SocialNetwork.py
+++ SocialNetwork.py
@@ -3,11 +3,8 @@ import networkx as nx
 
 
 class SocialNetwork:
-    # Конструктор сети, countUser - количество пользователей в сети
     def __init__(self, countUser:int):
-        # Инициализация поля количества пользователей
         self.countUser = countUser
-        # Инициализация пустого графа
         self.graph = nx.Graph()
         for i in range(self.countUser):
             # Добавление в граф всех пользователей
@@ -15,33 +12,29 @@ class SocialNetwork:
         for i in range(self.countUser):
             # Инициализация поля, количество знакомых каждого пользователя
             neighbors = len(list(self.graph.neighbors(i)))
-            # Если количесто знакомых < 4
             if neighbors < 4:
                 # Инициализация поля допустимого количества связей
                 edges = random.randint(1, 4 - neighbors)
-                # Пока количество связей > 0
                 while edges > 0:
                     # Инициализация поля знакомого пользоватедя
                     neighbor = random.randint(0, self.countUser-1)
                     # Если знакомый - это сам пользователь, или количество соседей > 3
                     if neighbor == i or len(list(self.graph.neighbors(neighbor))) > 3:
-                        # Пропустить
                         continue
-                    # Иначе
                     else:
                         # Добавить связь между пользователем и знакомым
                         self.graph.add_edge(i, neighbor)
                         # Уменьшить количество допустимых связей на 1
                         edges -= 1
 
-    # Метод взятия сгенерированного графа
+    # Метод получения сгенерированного графа
     def getGraph(self):
         return self.graph
 
-    # Метод взятия знакомых определенного пользователя, key - пользователь
+    # Метод получения знакомых определенного пользователя, key - пользователь
     def getNeighbors(self, key:int):
         return self.graph.neighbors(key)
 
-    # Метод взятия количества всех пользователей
+    # Метод получения количества всех пользователей
     def getNodes(self):
         return self.graph.nodes()
--- main.py
+++ main.py
@@ -14,18 +14,13 @@ countUser = 100
 # network - сгенерированная социальная сеть, user - исходных пользователь,
 # count - количество общих знакомых
 def find(network:SocialNetwork, user:int, count:int)->list:
-    # Инициализация поля списка друзей пользователя
     userFriends = list(network.getNeighbors(user))
-    # Инициализация поля списка незнакомцев с общими знакомыми
     strangers = []
-    # Инициализация поля размера социальной сести (исходный список)
     size = len(list(network.getNodes()))
     for man in range(size):
-        # Если man не является пользователем и не среди друзей пользователя
         if man != user and man not in userFriends:
             # Инициализация поля общих
             quantity = 0
-            # Инициализация поля знакомых man
             manFriend = list(network.getNeighbors(man))
             # Обход списка друзей пользователя
             for f in userFriends:
@@ -35,24 +30,17 @@ def find(network:SocialNetwork, user:int, count:int)->list:
                     quantity += 1
             # Если количество общих знакомых равно заданному
             if quantity == count:
-                # Добавить в исходный список man
                 strangers.append(man)
-    # Вернуть исходных список
     return strangers
 
-# Метод установки нужный цветов
+# Метод установки нужных цветов
 # lst - список незнакомцев с общими знакомыми, count - число пользоватейлей сети, 
 # user - исходный пользователь
 def listOfColor(lst:list, count:int, user:int)->list:
-    # Инициализация поля списка цветов пользователей
     lstColors = []
-    # Инициализация поля итерации
     i=0
-    # Пока итерация меньше поличества пользователей
     while i < count:
-        # Добавить в список цветов "фиолетовый"
         lstColors.append('purple')
-        #Увеличить итерацию на 1
         i += 1
     # Обход списка незнакомых
     for i in lst:
@@ -60,7 +48,6 @@ def listOfColor(lst:list, count:int, user:int)->list:
         lstColors[i]='r'
     # Цвет исходного пользователя - зеленый
     lstColors[user] = 'green'
-    # Вернуть список цветов
     return lstColors
 
 # Функция рисовки графа, network - социальная сеть,
@@ -68,7 +55,6 @@ def listOfColor(lst:list, count:int, user:int)->list:
 def main(network:SocialNetwork, ls):
     nx.draw(network.getGraph(), node_size=300, node_color=ls, edge_color='blue', font_color='yellow', font_weight='normal', font_size=7, with_labels=True)
 
-# Инициализация поля социальной сети
 net = SocialNetwork(countUser)
 # Инициализация поля количества пользователей социальной сети
 size = len(list(net.getNodes()))
@@ -82,18 +68,13 @@ for i in range(size):
         pl.clf()
         # Инициализация поля списка цветов
         ls = listOfColor(find(net, i, 1), countUser, i)
-        # Вызов метода для нарисования социальной сети
+        # Вызов метода для отрисовки социальной сети
         main(net, ls)
-        # Вызов метода для показа графа
         pl.show()
         # Вызов метода для паузы между переходами к следующему пользователю
         pl.pause(0.1)
     # Обработка возможных исключений
     except:
-        # Сообщение об ошибка
         print("Программа завершена до завершения алгоритма поиска")
-        # Завершение программы
         exit(0)
-
-# Сообщение об удачном выполнении программы
 print("Программа успешна завершена")
